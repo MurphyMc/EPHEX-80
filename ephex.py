3c5b8fee33ad48d063ef72e8dd7a279bb016019a
Murphy <murphy.mccauley@gmail.com>
2017-12-17 16:06:03 -0700

    Add simple driver program

---
+++ ephex.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2.7
+from ephex_core import *
+
+f = EPHEX()
+
+import sys, os
+
+fnum = 0
+while os.path.exists("%s.svg" % (fnum,)):
+  fnum += 1
+
+err_out("Starting with " + str(fnum))
+
+outfile = None
+
+def newfile ():
+  global fnum
+  global outfile
+  outfile = file("%s.svg" % (fnum,), "w")
+  err_out("Opening %s" % (fnum,))
+  fnum += 1
+
+def out (s):
+  if not outfile: newfile()
+
+  outfile.write(s)
+
+def closefile ():
+  global outfile
+  if outfile:
+    out(SVG_FOOTER)
+    outfile.close()
+    outfile = None
+
+def formfeed (dummy):
+  err_out("Form feed")
+  closefile()
+
+f.form_feed_callback = formfeed
+
+
+data = file(sys.argv[1], 'rb').read()
+f.feed_input(data)
+
+sys.stdout.write(f.save_to_string())
